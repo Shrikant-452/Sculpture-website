44f9552f23b7b0556d909833f693516bc1e3b56d
Rahul Birajdar <rbirajadar96@gmail.com>
2026-02-20 10:26:56 +0530

    adding sum fun

--- shoping-page/watchList.js
+++ shoping-page/watchList.js
@@ -16,28 +16,38 @@ function displayWatchListItem() {
   }
 
   watchlistItems.forEach((item) => {
-    container.innerHTML += `<div class="box1 " data-category="${item.category}" >
-                <a href="#"><img src="${item.img}" alt="" height="400" width="300" onclick="displayImagedetail('${item.id}', '${item.category}')"></a>
-                <div class="image-overlay">
-                    <i class="bi bi-heart" title="Add to Wishlist" onclick="addToWishlist('${item.id}', '${item.category}')"></i>
-                    <i class="bi bi-eye" title="View Details" onclick="displayImagedetail('${item.id}', '${item.category}')"></i>
-                    <i class="bi bi-cart" title="Add to Cart" onclick="displayImagedetail('${item.id}', '${item.category}')"></i>
+    container.innerHTML += `<div class="cart-card">
+                <!-- IMAGE -->
+                <div class="cart-img">
+                    <img src="${item.img}" alt="Product">
                 </div>
-                <div class="image-detail">
-                    <div class="product-textarea-title is-size-6 has-text-weight-medium ellipsis is-ellipsis-2">
-                        <a href="/shoping-page/order_Page.html" title="2&quot; Small Standing Lord Vishnu in Blessing Gesture | Copper Statue">${item.tital}</a>
-                    </div>
-                    <div class="product-textarea-subtitle is-size-7 ellipsis is-ellipsis-1 is-family-secondary is-uppercase">
-                        ${item.subtitle}
-                    </div>
+                <!-- DETAILS -->
+                <div class="cart-details">
+
+                <div class="top-row">
                     <div>
-                        <div class="is-flex is-flex-wrap-wrap is-align-items-center price-section">
-                            <div class="product-textarea-price is-size-6-touch is-size-5-desktop has-text-weight-medium mr-1 product-textarea-finalprice price">Rs.${item.price}
-                            </div>
-                            <div class="product-small-tag success has-text-black has-family-secondary ">Express Shipping</div>
-                        </div>
+                        <h2>${item.tital}</h2>
+                        <p class="category">${item.category}</p>
                     </div>
+                    <div class="price">Rs.${item.price}</div>
+                </div>
+
+                <p class="tax-price">Rs.${item.price}<br><span>INCLUSIVE OF ALL TAXES</span></p>
+                <!-- ACTIONS -->
+                <div class="action-buttons">
+                    <button type="button" class="remove-btn" onclick="removeItem('${item.id}','${item.category}')">Remove</button>
+                </div>
                 </div>
             </div>`;
   });
+}
+
+function removeItem(id, category) {
+  let watchlistItems = JSON.parse(localStorage.getItem("wishlistItems")) || [];
+  watchlistItems = watchlistItems.filter(
+    (item) => item.id !== Number(id) || item.category !== category,
+  );
+  localStorage.setItem("wishlistItems", JSON.stringify(watchlistItems));
+  alert("Item removed from watchlist");
+  displayWatchListItem();
 }
